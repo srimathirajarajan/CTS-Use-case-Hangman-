cb8fbcee2c686761b61988ac4ba63f3c35bfe3b4
Priyzzz <109037877+Priyzzz@users.noreply.github.com>
2024-04-04 16:09:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+
+def take_user_guess():
+    guess = input("Guess a letter: ").lower()
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter a single alphabetic character.")
+        return take_user_guess()
+    return guess
+
 def hangman():
     secret_word = choose_random_word()
     guessed_letters = []
